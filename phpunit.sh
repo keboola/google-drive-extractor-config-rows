685e7be74d7a4dd40fad0776255c279e73432fa0
Miroslav Cillik <miro@keboola.com>
2017-01-04 15:45:33 +0100

    fix (phpunit.sh): temporarily remove test-reporter

--- phpunit.sh
+++ phpunit.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 composer install \
 && ./vendor/bin/phpcs --standard=psr2 -n --ignore=vendor --extensions=php . \
-&& ./vendor/bin/phpunit \
-&& ./vendor/bin/test-reporter
+&& ./vendor/bin/phpunit
