9be4ceb6b0802c0c2873c960520b8a87716dd380
Ondra Jodas <ondrej.jodas@keboola.com>
2020-12-03 09:49:38 +0100

    test for bug

--- tests/FunctionalTest.php
+++ tests/FunctionalTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Keboola\GoogleDriveExtractor\Tests;
 
+use Keboola\Csv\CsvWriter;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Yaml\Yaml;
@@ -102,6 +103,36 @@ class FunctionalTest extends BaseTest
         unlink($filePath);
     }
 
+    public function testMultipleHeader(): void
+    {
+        $filePath = __DIR__ . '/data/in/multiple_header.csv';
+        touch($filePath);
+        $csv = new CsvWriter($filePath);
+        $this->writeLineToCsvFile($csv, 1, 7);
+        $this->writeLineToCsvFile($csv, 2, 5);
+        $this->writeLineToCsvFile($csv, 3, 20);
+        $this->writeLineToCsvFile($csv, 4, 2);
+        $this->writeLineToCsvFile($csv, 5, 7);
+
+        $this->testFileName = 'multiple_header';
+        $this->testFile = $this->prepareTestFile($filePath, 'multiple_header');
+        $this->config = $this->makeConfig($this->testFile);
+
+        // leave the header as is
+        $this->config['parameters']['sheets'][0]['header']['rows'] = 2;
+
+        $process = $this->runProcess();
+        $this->assertEquals(0, $process->getExitCode(), $process->getErrorOutput());
+
+        $fileId = $this->config['parameters']['sheets'][0]['fileId'];
+        $sheetId = $this->config['parameters']['sheets'][0]['sheetId'];
+
+        $this->assertFileEquals(
+            __DIR__ . '/data/expectedFiles/multiple_header.csv',
+            $this->dataPath . '/out/tables/' . $this->getOutputFileName($fileId, $sheetId)
+        );
+    }
+
     private function runProcess(): Process
     {
         $fs = new Filesystem();
@@ -116,5 +147,14 @@ class FunctionalTest extends BaseTest
         $process->run();
 
         return $process;
+    }
+
+    private function writeLineToCsvFile(CsvWriter $csvWriter, int $rowNumber, int $countColumns): void
+    {
+        $headerLine = [];
+        for ($j = 0; $j < $countColumns; $j++) {
+            $headerLine[] = sprintf('Col_%s_%s', $rowNumber, $j);
+        }
+        $csvWriter->writeRow($headerLine);
     }
 }
